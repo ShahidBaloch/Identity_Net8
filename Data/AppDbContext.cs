38cf72c1c631930f5559694de0e117e1ebc07355
shahidbaloch <muhammadshahid6528@gmail.com>
2024-10-22 08:44:26 +0500

    Add DbContext and Create IdentityDb

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity_Net8.Data
+{
+    public class AppDbContext : IdentityDbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
